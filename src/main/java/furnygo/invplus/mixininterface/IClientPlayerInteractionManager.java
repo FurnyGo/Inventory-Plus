4ec3886dc744596b39f2ba8090621bd4356769f3
FurnyGo <68079109+FurnyGo@users.noreply.github.com>
2022-08-21 18:01:38 +0400

    .

--- src/main/java/furnygo/invplus/mixininterface/IClientPlayerInteractionManager.java
+++ src/main/java/furnygo/invplus/mixininterface/IClientPlayerInteractionManager.java
@@ -1,9 +1,4 @@
 package furnygo.invplus.mixininterface;
-//========================================
-//
-//     Original code in MeteorClient
-//
-//========================================
 public interface IClientPlayerInteractionManager {
     void syncSelected();
 }
